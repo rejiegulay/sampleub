a9aaad7aefa54f0661f3e17113bf079e10c24ffc
rejiegulay <gulayrejie@gmail.com>
2024-09-20 15:17:29 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -137,6 +137,24 @@
         items: 1,
         loop: true,
     });
+
+    // Page loading animation
+	$(window).on('load', function() {
+		if($('.cover').length){
+			$('.cover').parallax({
+				imageSrc: $('.cover').data('image'),
+				zIndex: '1'
+			});
+		}
+
+		$("#preloader").animate({
+			'opacity': '0'
+		}, 600, function(){
+			setTimeout(function(){
+				$("#preloader").css("visibility", "hidden").fadeOut();
+			}, 300);
+		});
+	});
     
 })(jQuery);
 
